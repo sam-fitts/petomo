a30aeec5364cf32371b2552ae477b9ad79d8a0b1
Sam Fitts <sam.fitts@mac.com>
2018-02-28 14:00:03 +0900

    problem with git, trying something

--- app/policies/animal_policy.rb
+++ app/policies/animal_policy.rb
@@ -1,19 +1,19 @@
-class AnimalPolicy < ApplicationPolicy
-  class Scope < Scope
-    def resolve
-      scope.all
-    end
+# class AnimalPolicy < ApplicationPolicy
+#   class Scope < Scope
+#     def resolve
+#       scope.all
+#     end
 
-    def update?
-    record.user == user
-    end
+#     def update?
+#     record.user == user
+#     end
 
-    def destroy?
-    record.user == user
-    end
+#     def destroy?
+#     record.user == user
+#     end
 
-    def create?
-    return true
-    end
-  end
-end
+#     def create?
+#     return true
+#     end
+#   end
+# end
--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -1,53 +1,53 @@
-class ApplicationPolicy
-  attr_reader :user, :record
+# trueclass ApplicationPolicy
+#   attr_reader :user, :record
 
-  def initialize(user, record)
-    @user = user
-    @record = record
-  end
+#   def initialize(user, record)
+#     @user = user
+#     @record = record
+#   end
 
-  def index?
-    false
-  end
+#   def index?
+#     true
+#   end
 
-  def show?
-    scope.where(:id => record.id).exists?
-  end
+#   def show?
+#     scope.where(:id => record.id).exists?
+#   end
 
-  def create?
-    false
-  end
+#   def create?
+#     true
+#   end
 
-  def new?
-    create?
-  end
+#   def new?
+#     create?
+#   end
 
-  def update?
-    false
-  end
+#   def update?
+#     true
+#   end
 
-  def edit?
-    update?
-  end
+#   def edit?
+#     update?
+#   end
 
-  def destroy?
-    false
-  end
+#   def destroy?
+#     true
+#   end
 
-  def scope
-    Pundit.policy_scope!(user, record.class)
-  end
+#   def scope
+#     Pundit.policy_scope!(user, record.class)
+#   end
 
-  class Scope
-    attr_reader :user, :scope
+#   class Scope
+#     attr_reader :user, :scope
 
-    def initialize(user, scope)
-      @user = user
-      @scope = scope
-    end
+#     def initialize(user, scope)
+#       @user = user
+#       @scope = scope
+#     end
 
-    def resolve
-      scope
-    end
-  end
-end
+#     def resolve
+#       scope
+#     end
+#   end
+# end
